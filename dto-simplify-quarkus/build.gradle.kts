e0323f68c5120aa83d71e6cf61e8c4930a10aee4
jkvn <ceepkev77@gmail.com>
2025-07-02 23:32:05 +0200

    Add Gradle build configuration and enhance DtoSchemaRegistry for Quarkus support

--- dto-simplify-quarkus/build.gradle.kts
+++ dto-simplify-quarkus/build.gradle.kts
@@ -1,3 +1,16 @@
-subprojects {
-    apply(plugin = "java-library")
+plugins {
+    id("java-library")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation("io.quarkus:quarkus-smallrye-openapi:$quarkusPlatformVersion")
+    implementation(project(":dto-simplify-openapi"))
 }
--- dto-simplify-quarkus/deployment/build.gradle.kts
+++ dto-simplify-quarkus/deployment/build.gradle.kts
@@ -7,10 +7,12 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation(project(":dto-simplify-openapi"))
     implementation(project(":dto-simplify-quarkus:runtime"))
 
     implementation(enforcedPlatform("$quarkusPlatformGroupId:$quarkusPlatformArtifactId:$quarkusPlatformVersion"))
 
+    implementation("io.swagger.core.v3:swagger-core:2.2.31")
     implementation("io.quarkus:quarkus-core-deployment")
     implementation("io.quarkus:quarkus-arc-deployment")
     implementation("io.quarkus:quarkus-smallrye-openapi-deployment")
