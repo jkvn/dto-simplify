5ddde2be363661048403d11c2a0a00fb4a54e874
jkvn <ceepkev77@gmail.com>
2025-07-04 16:18:03 +0200

    delete useless class

--- dto-simplify-spring/build.gradle.kts
+++ dto-simplify-spring/build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-autoconfigure:3.5.3")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
-    implementation("org.reflections:reflections:0.10.2")
 
     api(project(":dto-simplify-core"))
     api(project(":dto-simplify-openapi"))
