67cc3704c35a86b10621d68ee06f1464f0d047da
jkvn <ceepkev77@gmail.com>
2025-07-05 00:08:53 +0200

    Add push trigger for main branch in GitHub Actions publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ subprojects {
                 repositories {
                     maven {
                         name = "GitHubPackages"
-                        url = uri("https://maven.pkg.github.com/jkvn/DTOSimplify")
+                        url = uri("https://maven.pkg.github.com/jkvn/dto-simplify")
                         credentials {
                             username = project.findProperty("githubUser") as String? ?: System.getenv("GITHUB_ACTOR")
                             password = project.findProperty("githubToken") as String? ?: System.getenv("GITHUB_TOKEN")
