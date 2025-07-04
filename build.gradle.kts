b12e03c4618c6c339bf906e4c77b8f341ae01685
jkvn <ceepkev77@gmail.com>
2025-07-05 00:07:46 +0200

    Add GitHub Packages publishing configuration and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,34 @@ allprojects {
         mavenCentral()
     }
 }
+
+subprojects {
+    plugins.withId("java-library") {
+        apply(plugin = "maven-publish")
+
+        afterEvaluate {
+            extensions.configure<PublishingExtension>("publishing") {
+                publications {
+                    create<MavenPublication>("gpr") {
+                        from(components["java"])
+
+                        groupId = "at.jkvn.dtosimplify"
+                        artifactId = project.name
+                        version = rootProject.version.toString()
+                    }
+                }
+
+                repositories {
+                    maven {
+                        name = "GitHubPackages"
+                        url = uri("https://maven.pkg.github.com/jkvn/DTOSimplify")
+                        credentials {
+                            username = project.findProperty("githubUser") as String? ?: System.getenv("GITHUB_ACTOR")
+                            password = project.findProperty("githubToken") as String? ?: System.getenv("GITHUB_TOKEN")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
